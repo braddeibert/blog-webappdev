88f2e330419d112be80fe8a8718ed71a975907f7
Brad Deibert <bradeibert7@gmail.com>
2020-09-17 21:18:22 -0600

    fixed failing test - added reverse_lazy import

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(ListView):
